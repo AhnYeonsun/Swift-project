5a7d1f6562ecbd056eb3e11397ff8f569a94fb4a
AhnYeonsun <yeonsun0517@gmail.com>
2020-01-12 16:41:50 +0900

    Part1. Storyboard

---
+++ Basic_for_study/Basic_for_study/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Basic_for_study
+//
+//  Created by 안연선 on 2020/01/12.
+//  Copyright © 2020 sunny-til. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
