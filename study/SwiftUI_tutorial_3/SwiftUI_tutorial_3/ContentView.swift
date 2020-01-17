b7e2efaffa1fdc4e4e77cf1b808cb2b6b7a288cb
AhnYeonsun <yeonsun0517@gmail.com>
2020-01-17 15:44:16 +0900

    Rearrange files

---
+++ study/SwiftUI_tutorial_3/SwiftUI_tutorial_3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_tutorial_3
+//
+//  Created by 안연선 on 2020/01/17.
+//  Copyright © 2020 sunny-til. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
